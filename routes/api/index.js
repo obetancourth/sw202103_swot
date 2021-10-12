c96edebce9af3f5b66e0ec256f3553e4b8275dac
Orlando J Betancourth <orlando.betancourth@gmail.com>
2021-10-11 18:50:31 -0600

    First Con express Raw

---
+++ routes/api/index.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', (req, res, next)=>{
+    res.status(200).json({"msg":"Hola Mundo"});
+  }
+);
+
+router.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
+
+router.get("/hello", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+router.get("/add/:int1/:int2", (req, res, next) => {
+  const { int1, int2 } = req.params;
+  const result = parseInt(int1) + parseInt(int2);
+  return res.status(200).json(
+    { int1: parseInt(int1), int2: parseInt(int2), resultado: result }
+  );
+});
+
+router.post("/new", (req, res, next) => {
+  const { email, pswd, secret } = req.body;
+  // vamos a agregarlos a alguna persistencia -- DynamoDB  --MongoDb Atlas
+  return res.status(200).json(
+    {
+      "email": email,
+      "password": pswd,
+      "secret": secret,
+      "time": new Date().getTime() //new Date().toISOString()
+    }
+  );
+});
+
+router.put("/update/:id", (req, res, next) => {
+  const { id } = req.params;
+  const { email, pswd, secret } = req.body;
+
+  // actualizar el recursos de la persistencia
+  return res.status(200).json(
+    {
+      "id": id,
+      "email": email,
+      "password": pswd,
+      "secret": secret,
+      "time": new Date().getTime() //new Date().toISOString()
+    }
+  );
+});
+
+router.delete("/delete/:id", (req, res, next) => {
+  const { id } = req.params;
+  // en la persistencia se debe borrar el recurso identificado
+  return res.status(200).json(
+    {
+      "id": id,
+      "time": new Date().getTime() //new Date().toISOString()
+    }
+  );
+});
+
+router.post("/conjunto", (req, res, next) => {
+  const rangostr = req.body.rango;
+  let arrTuplas = rangostr.replace(/[\[\]]/g, "").split(";");
+  //eliminar los corechetes
+  //separar por ,
+  // generar el conjunto unico
+  // devolver el json
+  return res.status(200).json({ rango: arrTuplas });
+});
+
+
+module.exports = router;
